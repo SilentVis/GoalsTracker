74f256c2300892331280c7d9c0826546cb5a6bf2
Serhii Tereshchenko <greywolf1396@gmail.com>
2022-11-15 12:30:08 +0200

    test

---
+++ src/gt-web/src/app/toolbar/toolbar.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppToolbarComponent } from './app-toolbar/app-toolbar.component';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppToolbarComponent
+  ],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
+  exports:[
+    AppToolbarComponent
+  ]
+})
+export class ToolbarModule { }
